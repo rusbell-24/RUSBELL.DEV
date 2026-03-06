23efe939ea29482633476c8fbaaf93aae6a4b01c
Rusbell Ruiz <rusbell.ruiz.p@gmail.com>
2026-03-06 18:24:13 -0500

    refactor: code splitting in mainLayout

--- src/components/layout/MainLayout/MainLayout.tsx
+++ src/components/layout/MainLayout/MainLayout.tsx
@@ -1,11 +1,17 @@
-import About from "../../../sections/About/About"
-import Curriculum from "../../../sections/Curriculum/Curriculum"
-import Portfolio from "../../../sections/Portfolio/Portfolio"
-import ProfileCard from "../../../sections/ProfileCard/ProfileCard"
-import Skills from "../../../sections/Skills/Skills"
-import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
+import ProfileCard from "../../../sections/ProfileCard/ProfileCard"
+import Curriculum from "../../../sections/Curriculum/Curriculum"
+import Footer from "../Footer/Footer"
 import styles from "./MainLayout.module.css"
+
+import {lazy, Suspense} from "react";
+
+const About = lazy(() => import('../../../sections/About/About'));
+const Portfolio = lazy(() => import('../../../sections/Portfolio/Portfolio'));
+const Skills = lazy(() => import('../../../sections/Skills/Skills'));
+
+
+
 
 const MainLayout = () =>{
     return(
@@ -15,15 +21,22 @@ const MainLayout = () =>{
                 <section id="profile-card" className={`${styles.section} ${styles.profileCardSection}`}>
                     <ProfileCard />
                 </section>
-                <section id="about" className={`${styles.section} ${styles.aboutSection}`}>
-                    <About />
-                </section>
-                <section id="portfolio" className={`${styles.section} ${styles.portfolioSection}`}>
-                    <Portfolio />
-                </section>
-                <section id="skills" className={`${styles.section} ${styles.skillsSection}`}>
-                    <Skills />
-                </section>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <section id="about" className={`${styles.section} ${styles.aboutSection}`}>
+                        <About />
+                    </section>
+                </Suspense>
+
+                <Suspense fallback={<div>Loading...</div>}>
+                    <section id="portfolio" className={`${styles.section} ${styles.portfolioSection}`}>
+                        <Portfolio />
+                    </section>
+                </Suspense>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <section id="skills" className={`${styles.section} ${styles.skillsSection}`}>
+                        <Skills />
+                    </section>
+                </Suspense>
                 <section id="curriculum" className={`${styles.section} ${styles.curriculumSection}`}>
                     <Curriculum />
                 </section>
